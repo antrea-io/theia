0109c53ec8b9d1c9fb2264619f3ba834d6d991f9
Yongming Ding <dyongming@vmware.com>
2022-06-23 16:50:41 -0700

    Lint policy recommendation PySpark code with flake8
    Signed-off-by: Yongming Ding <dyongming@vmware.com>

--- plugins/policy-recommendation/policy_recommendation_utils.py
+++ plugins/policy-recommendation/policy_recommendation_utils.py
@@ -19,6 +19,7 @@ import json
 from re import sub
 import yaml
 
+
 def is_intstring(s):
     try:
         int(s)
@@ -26,25 +27,34 @@ def is_intstring(s):
     except ValueError:
         return False
 
+
 def get_IP_version(IP):
     return "v4" if type(ip_address(IP)) is IPv4Address else "v6"
+
 
 def camel(s):
     s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
     return s[0].lower() + s[1:] if s else ""
 
+
 def camel_dict(d):
     result = {}
     for key, value in d.items():
         if isinstance(value, list):
-            result[camel(key)] = list(map(
-                lambda x: camel_dict(x) if isinstance(x, dict) else x, value
-            ))
+            result[camel(key)] = list(
+                map(
+                    lambda x: camel_dict(x) if isinstance(x, dict) else x,
+                    value,
+                )
+            )
         elif isinstance(value, dict) and key != "match_labels":
             result[camel(key)] = camel_dict(value)
         elif value is not None:
             result[camel(key)] = value
     return result
 
+
 def dict_to_yaml(d):
-    return yaml.dump(yaml.load(json.dumps(camel_dict(d)), Loader=yaml.FullLoader))
+    return yaml.dump(
+        yaml.load(json.dumps(camel_dict(d)), Loader=yaml.FullLoader)
+    )
